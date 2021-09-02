d9f1d4d6e6003a52329c34ce3fcb61bf1e58ebe9
EC2 Default User <ec2-user@ip-172-31-36-216.sa-east-1.compute.internal>
2021-09-02 01:00:52 +0000

    Arvore insercao

--- src/classAVL.cpp
+++ src/classAVL.cpp
@@ -1,9 +1,3 @@
-/*
-Implementação da classe AVL
-(aproveitei estas linhas de código do exercício de pilhas. Então, claro, elas precisam ser alteradas para o código de árvore AVL)
-Deixei apenas para o caso de servirem para alguma lembrança.
-*/
-
 #include<iostream>
 #include<cstring>
 #include<string>
@@ -11,106 +5,176 @@ Deixei apenas para o caso de servirem para alguma lembrança.
 using namespace std;   // declaração global do espaço de nomes
 
 struct No_AVL {
-    int numTelefone;
-    string nome;
+
+    int chave;
     int fb; // Fator de Balanço
     No_AVL* esq;  // aponta subárvore esquerda
     No_AVL* dir;  // aponta subárvore direita
 };
 ///////////////////////  Implementação da classe AVL ////////////////////////////
-class AVL
-{
+class AVL{
+
 public:
-    int N; // num. de elementos na AVL          //O(1)
-    No_AVL* noRaiz;          //O(1)
-    AVL();          //O(1)
-    void libera() { //Libera a memória ocupada pela AVL          //O(n)
-        // for (int i=0; i<this->N; i++){          //O(n)
-        //     No_AVL* proxNode=this->nodeTopo->prox;              //O(1)        
-        //     delete(this->nodeTopo);          //O(1)
-        //     this->nodeTopo=proxNode;          //O(1)
-        // }        
-    } 
-    //Retorna o elemento situado na raiz da AVL
-    No_AVL* getRaiz(){          //O(1)
-        // if (this->noRaiz>0){          //O(1)
-        //     return this->noRaiz;          //O(1)
-        // } else {
-        //     return 0;          //O(1)
-        // }
-        return 0;
+    int N;
+    No_AVL* T; 
+    char hAum[2];
+    AVL();
+    
+   
+    void insere(No_AVL** T, No_AVL* pNo){
+        if (*T==NULL){
+            (*T)=pNo;
+            (**T).fb=0;
+            (**T).esq=NULL;
+            (**T).dir=NULL;
+            this->N++;
+            strcpy(hAum,"V\0");
+           // printf("T: %p, *T:%p, **T: %p, pNo:%p, *pNo: %p, T->chave %d\n", T, (*T), (**T), pNo, *pNo, (**T).chave);
+    		
+        } else if ((**T).chave>(*pNo).chave){
+            insere(&(**T).esq, pNo);
+            if (hAum[0]=='V'){
+                atualizaBalanceamentoNoEsq(T, hAum);
+            }
+    		
+        } else if ((**T).chave<(*pNo).chave){
+            insere(&(**T).dir, pNo);
+            if (hAum[0]=='V'){
+                atualizaBalanceamentoNoDir(T, hAum);
+            }
+        } else {
+            perror("Chave ja existente!");
+        }
     };
-    //Insere o elemento x na AVL
-    void insere(No_AVL* x){          //O(1)
-        // No_AVL* novoNodeTopo=x;          //O(1)
-        // No_AVL* nodeTopoAntigo;          //O(1)
-        // if(this->N>0){          //O(1)
-        //     nodeTopoAntigo=this->nodeTopo;          //O(1)
-        //     novoNodeTopo->prox=nodeTopoAntigo;          //O(1)
-        //     this->nodeTopo=novoNodeTopo;          //O(1)
-        // } else{
-        //     this->nodeTopo=x;          //O(1)
-        // }        
-        // this->N++;                           //O(1)
+    
+    void atualizaBalanceamentoNoEsq(No_AVL** T, char * hAum){
+        if ((**T).fb==1){
+            (**T).fb=0;
+            strcpy(hAum,"F\0");
+        } else if ((**T).fb==0){
+            (**T).fb=-1;
+    		
+        } else {
+            rotacaoDireita(T);
+            (**T).fb=0;
+            strcpy(hAum,"F\0");
+        }
     };
+    
+    void atualizaBalanceamentoNoDir(No_AVL** T, char * hAum){
+        if ((**T).fb==-1){
+            (**T).fb=0;
+            strcpy(hAum,"F\0");
+        } else if ((**T).fb==0){
+            (**T).fb=1;
+        }
+        else {
+            rotacaoEsquerda(T);
+            (**T).fb=0;
+            strcpy(hAum,"F\0");
+        }
+    };
+    
+    void rotacaoDireita(No_AVL** T){
+        No_AVL* noTemp;
+        noTemp=((No_AVL*)(**T).esq);
+        if ((char) noTemp->fb==-1){
+            rotacaoDireitaSimples(T);
+        } else {
+    		rotacaoEsquerdaSimples(&(*T)->esq);
+    		rotacaoDireitaSimples(T);
+        }
+    };
+    
+    
+    void rotacaoEsquerda(No_AVL** T){
+        No_AVL* noTemp;
+        noTemp=((No_AVL*)(**T).dir);
+    	if ((char) noTemp->fb==1){
+    		   rotacaoEsquerdaSimples(T);
+    	} else{
+    		rotacaoDireitaSimples(&(*T)->dir);
+    		rotacaoEsquerdaSimples(T);
+    	}
+    };
+    
+    void rotacaoDireitaSimples(No_AVL** T){
+        No_AVL* noTemp = (No_AVL*)(*T)->esq;
+        (*T)->esq=noTemp->dir;
+        noTemp->dir=*T;
+        atualizafbT(*T);
+        *T=noTemp;
+    };
+    
+    void rotacaoEsquerdaSimples(No_AVL** T){
+        No_AVL* noTemp = (*T)->dir;
+        (*T)->dir = noTemp->esq;
+        noTemp->esq=*T;
+        atualizafbT(*T);
+        *T=noTemp;
+    };
+        
+    void atualizafbT(No_AVL* T){
+     if(T->dir==NULL && T->esq==NULL){
+            T->fb=0;
+    	}else {
+            if (T->dir!=NULL && T->esq!=NULL){
+                if ((T->dir)->fb==0 && (T->esq)->fb==0){
+                    T->fb=0;
+    			} else {
+                    if ((T->dir)->fb!=0 && (T->esq)->fb==0) {
+                        T->fb=1;
+                    } else {
+                        if ((T->dir)->fb==0 && (T->esq)->fb!=0) T->fb=-1;
+                        else T->fb=0;
+                    }
+                }
+            } else {
+                if ((T->dir!=NULL && T->esq==NULL)) T->fb=1;
+                else T->fb=-1;
+            }
+        }
+    };
+    
+        
     //Exclui o elemento com o nome e número determinados
     No_AVL* exclui(string nome, int telefone){          //O(1)
-        // No_AVL* retorno=this->topo();          //O(1)
-        // No_AVL* novoNodeTopo;          //O(1)
-        // if(this->nodeTopo->prox>0){          //O(1)
-        //     novoNodeTopo=this->nodeTopo->prox;               //O(1)            
-        //     this->nodeTopo=novoNodeTopo;                       //O(1)
-        // } else {
-        //     this->nodeTopo=0;          //O(1)
-        // }  
-        // this->N--;             //O(1)
-        // return retorno;          //O(1)
+    
     };
     //Busca o elemento x na pilha
     No_AVL* getElemento(No_AVL* elemento){          //O(1)
-        // No_AVL* retorno=this->topo();          //O(1)
-        // No_AVL* novoNodeTopo;          //O(1)
-        // if(this->nodeTopo->prox>0){          //O(1)
-        //     novoNodeTopo=this->nodeTopo->prox;               //O(1)            
-        //     this->nodeTopo=novoNodeTopo;                       //O(1)
-        // } else {
-        //     this->nodeTopo=0;          //O(1)
-        // }  
-        // this->N--;             //O(1)
-        // return retorno;          //O(1)
+    
     };     
     //imprime todos os elementos da pilha (recebe o nó raiz ou aproveita o atributo raiz da própria classe)
-    void imprimeAVL(){          //O(n)
-        // No_AVL* proxNode=this->nodeTopo;            //O(1)
-        // for (int i=0; i<this->N; i++){                 //O(n)
-        //     if(proxNode->chrc>0)               //O(1)
-        //         printf("%c\t",proxNode->chrc);          //O(1)
-        //     proxNode=proxNode->prox;                                  //O(1)
-        // }
+    void imprime(No_AVL* T, int h){
+        
+        if(T != NULL){
+            imprime(T->esq, h+1);
+            printf(" %d ", T->chave);
+            imprime(T->dir, h+1);
+        }
     }
 };
 AVL::AVL(){              //O(1)
-    this->noRaiz=new No_AVL;          //O(1)
-    this->N=0;          //O(1)
+    this->T = NULL;          //O(1)
+    this->N = 0;          //O(1)
 };
 
-template<typename Agregado, typename Tipo, typename Tipo1, typename Tipo2>
-concept bool
-AVL_TAD = requires(Agregado a, Tipo t, Tipo1 t1, Tipo2 t2 )
-{
-// libera a memória ocupada pelos nós da AVL
-{ a.libera() };
-// obtem o elemento situado na raiz da AVL
-{ a.getRaiz() };
-// insere o elemento x
-{ a.insere(t) };
-// deleta o elemento x
-{ a.exclui(t1, t2) };
-// busca o elemento x
-{ a.getElemento(t) };
-// imprime os elementos da árvore (recebe o nó raiz ou aproveita o atributo raiz da própria classe)
-{ a.imprimeAVL() };
-};
-
-// verifica se os agregados satisfazem o conceito
-static_assert(AVL_TAD<AVL, No_AVL*, string, int>);
+int main(){
+    
+    AVL* avl_tree = new AVL();
+    No_AVL* X;
+    
+    No_AVL*T = NULL;
+    
+    for(int i; i< 7; i++){
+        X = new No_AVL();
+        X->chave = rand() % 20;
+        X->fb = 0;
+        avl_tree->insere(&T, X);
+    }
+    
+    avl_tree->imprime(T, 1);
+    
+    
+}
